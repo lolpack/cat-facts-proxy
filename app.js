8c6f205ce2cdf7ea0dbae988bd591f3c7cfeb14c
Aaron Pollack <pollackaaron@gmail.com>
2017-03-20 21:40:58 -0700

    Add CORS headers

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ var express = require('express');
 var request = require('request');
 var app = express()
 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
+
 app.get('/', function (req, res) {
   request('http://catfacts-api.appspot.com/api/facts', function (error, response, body) {
     res.send(body)
